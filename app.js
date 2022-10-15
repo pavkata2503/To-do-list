28b6d46805baa651426e32965ea5b5d00105ef52
pavkata2503 <72630031+pavkata2503@users.noreply.github.com>
2022-10-15 18:50:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+/* This is the code that is executed when the button is clicked. */
+document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#taskInput').value.length == 0) {
+        alert("Please Enter a Task")
+    }
+    else {
+        document.querySelector('#tasks').innerHTML += `
+    <div class="task">
+        <span id="taskname">
+            ${document.querySelector('#taskInput').value}
+        </span>
+        <button class="delete">
+            <i class="far fa-trash-alt"></i>
+        </button>
+    </div>
+`;
+
+ /* Adding an event listener to the delete button. */
+        var current_tasks = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+    document.querySelector('#taskInput').value = '';
+}
